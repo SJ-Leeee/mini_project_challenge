2d26fdbdb678f8caac6bdb7617add0f63088d2b7
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-09 20:52:19 +0900

    feat : 챌린지 카드 수정

--- app/routes/rendering_routes.py
+++ app/routes/rendering_routes.py
@@ -1,13 +1,23 @@
 from flask import Blueprint, request, jsonify, current_app, render_template
+from app.services.auth_service import auth_token
 
 rendering_bp = Blueprint("rendering", __name__)
 
 @rendering_bp.route("/", methods=["GET"])
 def main():
-    testuser=None
-    #testuser="aaa"
+    token = request.cookies.get("access_token")
+    is_valid, result = auth_token(token)
 
-    return render_template('main_page.html', current_user_nickname=testuser)
+    print(is_valid)
+    print(result)
+
+    if not is_valid:
+        return render_template('main_page.html')
+
+    db = current_app.config["DB"]
+    user = db["users"].find_one({"_id":result})
+
+    return render_template('main_page.html', current_user_nickname=user["nickname"])
 
 @rendering_bp.route("/register", methods=["GET"])
 def signup():
