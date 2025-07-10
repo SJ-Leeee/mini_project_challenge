31b1497c443dd4a7f971e27145fae0ff215bb8be
leeseungjun <cjstkrak@gmail.com>
2025-07-10 10:51:19 +0900

    fix: 네임수정

--- app/routes/file_routes.py
+++ app/routes/file_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 
-from app.utils.s3_func import upload_file_to_s3, delete_file_from_s3
+from app.utils.s3_func import upload_file_to_s3, delete_s3_file_by_url
 
 file_bp = Blueprint("file", __name__)
 
@@ -28,7 +28,7 @@ def delete_image():
         return jsonify({"error": "file_url required"}), 400
 
     try:
-        delete_file_from_s3(file_url)
+        delete_s3_file_by_url(file_url)
 
         return jsonify({"message": "삭제 성공"}), 200
     except Exception as e:
