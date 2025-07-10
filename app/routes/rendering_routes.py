8746dfd94e65dd3563aa0b37736e89da01c10a7d
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-10 10:45:12 +0900

    feat: mypage

--- app/routes/rendering_routes.py
+++ app/routes/rendering_routes.py
@@ -11,6 +11,9 @@ from app.services.challenge_service import (
 from app.services.record_service import (
     get_all_record_by_challenge_id,
 )
+from app.models.challenge_model import (
+    get_challenge_by_id,
+)
 
 
 rendering_bp = Blueprint("rendering", __name__)
@@ -49,24 +52,22 @@ def signup():
 
 @rendering_bp.route("/mypage", methods=["GET"])
 def mypage():
-    data = get_challenges_service(0, True)
+    token = request.cookies.get("access_token")
+    is_valid, _id = auth_token(token)
+
+    if not is_valid:
+        render_template("main_page.html", message=_id)
+        
+    db = current_app.config["DB"]
+    user = db["users"].find_one({"_id":ObjectId(_id)})
+
+    current_user_id = user["_id"]
+    current_user = user["nickname"]
+
+    data = get_challenges_service(0, False, current_user_id)
     results = []
 
     for challenge in data:
-        records_data = []
-
-        records = get_all_record_by_challenge_id(challenge["_id"])
-
-        for record in records:
-            month = int(record["created_date"][:2])
-            day = int(record["created_date"][2:])
-
-            record_date = date(datetime.now().year, month, day)
-
-            if (7 > (datetime.now().date() - record_date).days):
-                records_data.append((datetime.now().date() - record_date).days)
-
-        print(records_data)
         results.append({
             "id": challenge['_id'],
             "public": challenge['is_public'],
@@ -74,10 +75,9 @@ def mypage():
             "likes": challenge['like_count'],
             "days_from_start": (datetime.now() - challenge['start_date']).days,
             "days_until_end": (challenge['end_date'] - datetime.now()).days,
-            "records_data": records_data.sort()
         })
 
-    return render_template('mypage.html')
+    return render_template('mypage.html', current_user_nickname=current_user, card_datas=results)
 
 @rendering_bp.route("/challenge/<challenge_id>", methods=["GET"])
 def render_challenge_detail(challenge_id):
